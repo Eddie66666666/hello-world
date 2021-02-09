fe32b265cdf7669e547fe64ce58c80a2a9d5f519
Eddie66666666 <78803888+Eddie66666666@users.noreply.github.com>
2021-02-09 23:19:09 +0800

    Add files via upload
    2.6训练文件

---
+++ 训练2.6.cpp
@@ -0,0 +1,48 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int num=0;
+//	for(printf("love computer");num!=6;)
+//	{
+//		scanf("%d",&num);
+//	}
+//	
+//	
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	const int FIRST_OZ = 46;
+//	const int NEXT_OZ = 20;
+//	int ounces,cost;
+//	
+//	printf(" ounces cost \n");
+//	for( ounces = 1,cost = FIRST_OZ; ounces < 16; ounces++,cost += NEXT_OZ)
+//	{
+//		printf(" %d:     %d:\n",ounces,cost);
+//	}
+//	
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
