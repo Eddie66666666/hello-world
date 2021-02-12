6c84bd6d3f0e5e7778f9a7eb0218df10b3ce7449
Eddie66666666 <78803888+Eddie66666666@users.noreply.github.com>
2021-02-12 23:30:32 +0800

    Add files via upload
    2.12号训练

---
+++ 2.12.cpp
@@ -0,0 +1,77 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int n=1;
+//    int i=0;
+//    int sum=4;
+//    for(i=0;i<5;i++)
+//{   
+//	sum=(sum/4)*5+1;
+//}
+//printf("%d",sum);
+//	
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//    int x
+//    ,i=0,j=1;
+//    while(i<5){
+//        x=4*j;
+//        for(i=0;i<5;i++)
+//        {
+//            if(x%4!=0){break;}
+//            x=(x/4)*5+1;
+//        }
+//        j++;
+//    }
+//    printf("%d\n",x);
+//    
+//    return 0;
+//}
+//猴子偷桃问题 
+//#include <stdio.h>
+//int main()
+//{
+//	int n=1;
+//	while(n)
+//	{
+//		int i=0;
+//		for(i=0;i<5;i++)
+//		{
+//			
+//		}
+//	}
+//	
+//	
+//	
+//	return 0;
+//}
+
+//寻找数问题、
+//#include <stdio.h>
+//void print(int i)
+//{
+//	printf("The number is %d",i);
+//} 
+//int main()
+//{
+//	int i=10;
+//	int a=809;
+//	for(i=10;i<100;i++)
+//	{
+//		int b=0;
+//		b=a*i;
+//		if(b>1000&&b<10000&&8*i<100&&9*i>100)
+//		{
+//			print(i);
+//		}
+//	}
+//	
+//	
+//	
+//	return 0;
+//} 
